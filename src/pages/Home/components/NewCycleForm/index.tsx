7a8475ad4818f14e49ec94da596f42527357a3ef
lesantosx <leticia.santos1997@outlook.com>
2024-10-31 22:42:45 -0300

    Remove tag

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -23,13 +23,6 @@ export function NewCycleForm() {
         {...register('task')}
       />
 
-      <datalist id="task-suggestions">
-        <option value="Projeto 1" />
-        <option value="Projeto 2" />
-        <option value="Projeto 3" />
-        <option value="Banana" />
-      </datalist>
-
       <label htmlFor="minutesAmount">durante</label>
       <MinutesAmountInput
         type="number"
